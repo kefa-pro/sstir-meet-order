0e41d13fa67adbb851dbd7bea7b1dcdcc7965cb4
liuzhuang <70458055@qq.com>
2020-05-11 13:49:35 +0800

    :construction: modify cookie domain

--- src/config/index.js
+++ src/config/index.js
@@ -7,31 +7,36 @@ const getBaseUrl = env => {
     case "dev":
       return {
         baseUrl: "http://172.16.70.122:30000/vedio/",
-        homepageUrl: "http://10.10.11.165:8222/cas/login?service=http://localhost:8080"
+        homepageUrl: "http://10.10.11.165:8222/cas/login?service=http://localhost:8080",
+        domain: '.apps.datadrivecloud.com'
       };
     // production
     case "prod":
       return {
         baseUrl: "http://apigate.sstir.cn/vedio/",
-        homepageUrl: "http://cas.sstir.cn/cas/login?service=http://meet3.cloud.sstir.cn"
+        homepageUrl: "http://cas.sstir.cn/cas/login?service=http://meet3.cloud.sstir.cn",
+        domain: '.sstir.cn'
       };
     case "qa":
       return {
         baseUrl: "http://vedio-meeting-sstir-pre.apps.datadrivecloud.com/vedio/",
-        homepageUrl: "http://cas.sstir.cn/cas/login?service=http://gateway-sstir-pre.apps.datadrivecloud.com/basic/cas/login/meeting"
+        homepageUrl: "http://cas.sstir.cn/cas/login?service=http://gateway-sstir-pre.apps.datadrivecloud.com/basic/cas/login/meeting",
+        domain: '.apps.datadrivecloud.com'
       };
     // default: dev
     default:
       return {
         baseUrl: "http://apigate.sstir.cn/vedio/",
-        homepageUrl: "http://cas.sstir.cn/cas/login?service=http://meet3.cloud.sstir.cn"
+        homepageUrl: "http://cas.sstir.cn/cas/login?service=http://meet3.cloud.sstir.cn",
+        domain: '.apps.datadrivecloud.com'
       };
   }
 };
 
 const appConfig = {
   baseUrl: getBaseUrl(env).baseUrl,
-  homepageUrl: getBaseUrl(env).homepageUrl
+  homepageUrl: getBaseUrl(env).homepageUrl,
+  domain: getBaseUrl(env).domain
 };
 
 export default appConfig;
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,20 @@
 // 对Cookie的操作
 // 这里没有写过期时间，所以Cookie的有效期都是Session，一旦关闭浏览器就过期
+import appConfig from '@/config';
+const domain = appConfig.domain;
 
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
-const TokenKey = 'token'
+const TokenKey = 'token';
 
-export function getToken () {
-  return Cookies.get(TokenKey)
+export function getToken() {
+	return Cookies.get(TokenKey);
 }
 
-export function setToken (token) {
-  return Cookies.set(TokenKey, token)
+export function setToken(token) {
+	return Cookies.set(TokenKey, token, { domain });
 }
 
-export function removeToken () {
-  return Cookies.remove(TokenKey)
+export function removeToken() {
+	return Cookies.remove(TokenKey);
 }
